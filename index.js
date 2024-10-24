f1eed504834f4014d4a5b3a99f10a36e08b4b89f
StevenLomon <steven.lennartsson@gmail.com>
2024-10-24 07:14:08 +0200

    Imports done

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+const process = require('process');
+const sqlite3 = require('sqlite3').verbose(); // Verbose for more detailed logs in local development
+const bcrypt = require('bcrypt'); // For password hashing and verification
 const express = require('express');
+const flash = require('connect-flash');
+const session = require('express-session');
+const { Sequelize, DataTypes, ValidationError } = require('sequelize');
+require('dotenv').config();
 
+const sequelize = Sequelize({
+    dialect: 'sqlite',
+    storage: './finance.db'
+})
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+console.log(`Secret key: ${process.env.SECRET_KEY}`) // The type of rabbit ears we use is important!
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000')
-});
+app.use(flash());
+app.use(session({secret: process.env.SECRET_KEY, resave: false, saveUninitialized: true }))
+
+function loginRequired(req, res, next) {
+    if (!req.session.user_id) {
+      return res.redirect('/login');
+    }
+    next();
+  }
+
